6ff5ac3fea3cd68a5c0dceaf2dfde5aa921364a8
MinJeong Hong <147925385+mindaaaa@users.noreply.github.com>
2025-10-22 15:39:26 +0900

    feat(app): 자동차 경주 게임 통합
    - 입력/검증/게임 & 진행/결과 출력 통합

--- src/App.js
+++ src/App.js
@@ -1,5 +1,54 @@
+import InputView from './view/InputView.js';
+import OutputView from './view/OutputView.js';
+import InputValidator from './view/validator/InputValidator.js';
+import CarValidator from './domain/validator/CarValidator.js';
+import RoundValidator from './service/validator/RoundValidator.js';
+import RacingGame from './service/RacingGame.js';
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      const carNames = await this.#readAndValidateCarNames();
+      const rounds = await this.#readAndValidateRounds();
+
+      this.#playGame(carNames, rounds);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async #readAndValidateCarNames() {
+    const carNames = await InputView.readCarNames();
+
+    InputValidator.validateArrayNotEmpty(carNames);
+    CarValidator.validateCarNames(carNames);
+
+    return carNames;
+  }
+
+  async #readAndValidateRounds() {
+    const rounds = await InputView.readRounds();
+
+    InputValidator.validateNumeric(rounds);
+    RoundValidator.validateInteger(rounds);
+    RoundValidator.validateMinimum(rounds);
+
+    return rounds;
+  }
+
+  #playGame(carNames, rounds) {
+    const game = new RacingGame(carNames);
+
+    OutputView.printResultHeader();
+
+    for (let i = 0; i < rounds; i++) {
+      game.playRound();
+      OutputView.printRoundResult(game.cars);
+    }
+
+    const winners = game.getWinners();
+    OutputView.printWinners(winners);
+  }
 }
 
 export default App;
